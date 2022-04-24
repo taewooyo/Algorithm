8bc9f42e0c5d4f5d236e466406cb6a5f5c1549f7
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-04-24 15:51:01 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
@@ -1,152 +1,150 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
-#define MAX 51
-using namespace std;
 
+using namespace std;
+#define NM 55
+
+int dirs[8][2] = {
+    {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+int a[NM][NM], groom[NM][NM], next_groom[NM][NM];
 int n, m;
-int grid[MAX][MAX], groom[MAX][MAX], nextGroom[MAX][MAX];
-int dx[] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
-int dy[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
-int cdx[] = {-1, -1, 1, 1};
-int cdy[] = {-1, 1, 1, -1};
-void input() {
+void Input() {
     cin >> n >> m;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> grid[i][j];
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+}
+
+void Make_groom() {
+    groom[n-1][0] = 1;
+    groom[n-1][1] = 1;
+    groom[n-2][0] = 1;
+    groom[n-2][1] = 1;
+}
+
+void Move(int dir, int s) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            next_groom[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(groom[i][j]) {
+                int ny = ((i + s * dirs[dir][0])) % n;
+                int nx = ((j + s * dirs[dir][1])) % n;
+                if(ny < 0) ny += n;
+                if(nx < 0) nx += n;
+                next_groom[ny][nx] = 1;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            groom[i][j] = next_groom[i][j];
+        }
+    }
+}
+
+void Grow() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(groom[i][j]) a[i][j]++;
+        }
+    }
+}
+
+inline bool InRange(int y, int x) {
+    return 0 <= y && y < n && 0 <= x && x < n;
+}
+
+void DiagonalGrow() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(groom[i][j]) {
+
+                int count = 0;
+                for (int d = 1; d < 8; d += 2) {
+                    int ny = i + dirs[d][0];
+                    int nx = j + dirs[d][1];
+                    if(InRange(ny, nx) && a[ny][nx] > 0) count++;
+                }
+                a[i][j] += count;
+            }
+        }
+    }
+}
+
+void UpdateGroom() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            next_groom[i][j] = 0;
+        }
+    }    
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] >= 2 && groom[i][j] == 0) {
+                next_groom[i][j] = 1;
+                a[i][j] -= 2;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            groom[i][j] = next_groom[i][j];
+        }
+    }
 }
 
 void print() {
-    cout << "\nwater\n";
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << grid[i][j] << ' ';
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << ' ';
         }
-        cout << "\n";
+        cout << '\n';
     }
-}
-
-void makeGroom() {
-    for(int i = n-2; i < n; i++)
-        for(int j = 0; j < 2; j++)
-            groom[i][j] = 1;
-}
-
-pair<int, int> nextPos(int x, int y, int d, int s) {
-    // int nx = (x + dx[d] * s + n * s) % n;
-    // int ny = (y + dy[d] * s + n * s) % n;
-    for(int i = 0; i < s; i++) {
-        x += dx[d];
-        y += dy[d];
-
-        if(x == n) x = 0;
-        else if(x == -1) x = n - 1;
-        if(y == n) y = 0;
-        else if(y == -1) y = n - 1;
-    }
-
-    return make_pair(x, y);
-}
-
-void move(int d, int s) {
-    fill(&nextGroom[0][0], &nextGroom[0][0] + MAX*MAX, 0);
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            if(groom[i][j]) {
-                int x, y;
-                tie(x, y) = nextPos(i, j, d, s);
-                nextGroom[x][y] = 1;
-            }
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            groom[i][j] = nextGroom[i][j];
-}
-
-void increaseWater() {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(groom[i][j]) {
-                grid[i][j] += 1;
-            }
+    cout << '\n';
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << groom[i][j] << ' ';
         }
+        cout << '\n';
     }
 }
 
-bool inRange(int x, int y) {
-    return 0 <= x && x < n && 0 <= y && y <= n;
-}
-
-int getPos(int x, int y) {
-    int cnt = 0;
-    for(int i = 0; i< 4; i++) {
-        int nx = x + cdx[i], ny = y + cdy[i];
-        if(inRange(nx, ny) && grid[nx][ny] > 0) {
-            cnt++;
-        }
-    }
-    return cnt;
-}
-
-void copyWater() {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(groom[i][j]) {
-                int waterCnt = getPos(i, j);
-                grid[i][j] += waterCnt;
-            }
-        }
-    }
-}
-
-void updateGroom() {
-    fill(&nextGroom[0][0], &nextGroom[0][0] + MAX*MAX, 0);
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(grid[i][j] >= 2 && groom[i][j] == 0) {
-                nextGroom[i][j] = 1;
-                grid[i][j] -= 2;
-            }
-        }
-    }
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            groom[i][j] = nextGroom[i][j];
-}
-
-void simulate(int d, int s) {
-    // 구름 움직이기
-    move(d, s);
-    // 바구니 물의양 +1 증가
-    increaseWater(); 
-    // 물복사 시전
-    copyWater();
-    // 구름 생성 및 물의양 줄어들이기
-    updateGroom();
-}
-
-void process() {
-    makeGroom();
-    while(m--) {
+void Pro() {
+    Make_groom();
+    for(int rep = 1; rep <= m; rep++) {
         int d, s;
         cin >> d >> s;
-        simulate(d, s);
+    
+        Move(d - 1, s);    
+        Grow();
+        DiagonalGrow();
+        UpdateGroom();
     }
 
     int ans = 0;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            if(grid[i][j]) ans += grid[i][j];
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            ans += a[i][j];
+        }
+    }
     cout << ans;
 }
 
 int main() {
-    input();
-    process();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    Input();
+    Pro();
+    return 0;
 }
