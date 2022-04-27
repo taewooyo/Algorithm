3a473e7875d29708f2d3285d9c884c7625a22b08
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-04-27 14:12:27 +0900

    Update 마법사 상어와 토네이도.cc

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.cc
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.cc
@@ -1,100 +1,95 @@
 #include <iostream>
-#include <cstring>
-#include <cmath>
 #include <algorithm>
 #include <queue>
+#include <cmath>
+#include <vector>
+#define NM 505
+
 using namespace std;
-struct Node {
-    int x, y, dir;
+typedef long long int ll;
+
+int a[NM][NM], b[5][5];
+int y, x;
+int n, ans, len = 1, dir = 0;
+int dirs[4][2] = { {0, -1}, {1, 0}, {0, 1}, {-1, 0} };
+int ratioDust[5][5] = {
+    {0, 0, 2, 0, 0},
+    {0, 10, 7, 1, 0},
+    {5, 0, 0, 0, 0},
+    {0, 10, 7, 1, 0},
+    {0, 0, 2, 0, 0}
 };
 
-int dx[] = {0, 1, 0, -1};
-int dy[] = {-1, 0, 1, 0};
-int wx[4][9] = {
-    {-1, 1, -1, 1, -1, 1, -2, 2, 0}, // left
-    {-1, -1, 0, 0, 1, 1, 0, 0, 2}, // down
-    {-1, 1, -1, 1, -1, 1, -2, 2, 0}, // right
-    {1, 1, 0, 0, -1, -1, 0, 0, -2} // up
-};
-int wy[4][9] = {
-    {1, 1, 0, 0, -1, -1, 0, 0, -2}, // left
-    {-1, 1, -1, 1, -1, 1, -2, 2, 0}, // down
-    {-1, -1, 0, 0, 1, 1, 0, 0, 2}, // right
-    {-1, 1, -1, 1, -1, 1, -2, 2, 0} // up
-};
+void Rotate() { // 좌 하 우 상 // 반시계
+    for (int i = 0; i < 5; i++) for (int j = 0; j < 5; j++) b[i][j] = 0;
 
-int percent[] = {1, 1, 7, 7, 10, 10, 2, 2, 5};
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            b[i][j] = ratioDust[j][5 - i - 1];
+        }
+    }
 
-int N; int map[500][500];
-int answer;
-Node wind;
-
-void input() {
-    cin >> N;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            cin >> map[i][j];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            ratioDust[i][j] = b[i][j];
         }
     }
 }
 
-void spread() {
-    int value = map[wind.x][wind.y];
-    for(int i = 0; i < 9; i++) {
-        int x = wind.x + wx[wind.dir][i];
-        int y = wind.y + wy[wind.dir][i];
-
-        int sand = (value * percent[i]) / 100;
-        map[wind.x][wind.y] -= sand;
-
-        if(x < 0 || y < 0 || x >= N || y >= N) {
-            answer += sand;
-            continue;
+void Input() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
         }
-        map[x][y] += sand;
-    }
+    }    
+}
 
-    // alpha 처리
-    int x = wind.x + dx[wind.dir];
-    int y = wind.y + dy[wind.dir];
-    if(x < 0 || y < 0 || x >= N || y >= N) {
-        answer += map[wind.x][wind.y];
-        map[wind.x][wind.y] = 0;
-        return;
+inline bool InRange(int y, int x) {
+    return 1 <= y && y <= n && 1 <= x && x<= n;
+}
+
+void AddDust(int y, int x, int dust) {
+    if(!InRange(y, x)) ans += dust;
+    else a[y][x] += dust;
+}
+
+void Move() {
+    y += dirs[dir][0];
+    x += dirs[dir][1];
+    int addDust = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int dust = a[y][x] * ratioDust[i][j] / 100;
+            AddDust(y + i - 2, x + j - 2, dust);
+            addDust += dust;
+        }
     }
-    else {
-        map[x][y] += map[wind.x][wind.y];
-        map[wind.x][wind.y] = 0;
+    AddDust(y + dirs[dir][0], x + dirs[dir][1], a[y][x] - addDust);
+}
+
+void MoveTornado() {
+    y = n / 2 + 1, x = n / 2 + 1;
+    while(!(y == 1 && x == 1)) {
+        for (int i = 0; i < len; i++) {
+            Move();
+            if(y == 1 && x == 1) break;
+        }
+        dir = (dir + 1) % 4;
+        Rotate();
+        if (dir == 0 || dir == 2) len++;
     }
 }
 
-void move_wind(int length) {
-    for(int i = 0; i < length; i++) {
-        wind.x = wind.x + dx[wind.dir];
-        wind.y = wind.y + dy[wind.dir];
-        spread();
-    }
-}
-
-void pro() {
-    wind.x = N / 2; wind.y = N / 2; wind.dir = 0;
-
-    int length = 1;
-    for(int i = 1; i < N; i++) {
-        for(int j = 1; j <= 2; j++) {
-            move_wind(length);
-            wind.dir = (wind.dir + 1) % 4;       
-        }
-        length++;
-    }
-
-    move_wind(length - 1);
-    cout << answer << '\n';
+void Pro() {
+    MoveTornado();
+    cout << ans;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    input();
-    pro();
+    cin.tie(nullptr);
+    Input();
+    Pro();
+    return 0;
 }
